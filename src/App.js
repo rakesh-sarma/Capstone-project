a6727e4a7584f28f4185bf61e0f02fdb381a4a0b
Rakesh Sarma <rakeshsarma033@gmail.com>
2023-02-08 16:56:53 +0530

    added updates section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css'
 import MainDash from './components/MainDash/MainDash';
 import Sidebar from './components/sidebar/Sidebar';
-
+import RightSide from './components/RightSide/RightSide';
 function App() {
   return (
     <div className="App">
         <div className="AppGlass">
           <Sidebar/>
        <MainDash/>
+       <RightSide/>
         </div>
     </div>
   );
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -5,7 +5,9 @@ import {
     UilPackage,
     UilChart,UilUsdSquare,UilMoneyWithdrawal
   } from "@iconscout/react-unicons";
-
+  import img1 from '../imgs/img1.png'
+  import img2 from '../imgs/img2.png'
+  import img3 from '../imgs/img3.png'
 // Sidebar Data
 export const SidebarData = [
     {
@@ -82,4 +84,27 @@ export const CardsData = [
         ],
     },
 ]
+//Recent Update Data
 
+
+export const UpdatesData = [
+    {
+      img: img1,
+      name: "Andrew Thomas",
+      noti: "has ordered Apple smart watch 2500mh battery.",
+      time: "25 seconds ago",
+    },
+    {
+      img: img2,
+      name: "James Bond",
+      noti: "has received Samsung gadget for charging battery.",
+      time: "30 minutes ago",
+    },
+    {
+      img: img3,
+      name: "Iron Man",
+      noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
+      time: "2 hours ago",
+    },
+  ];
+  
