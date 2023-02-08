9bad82a32df00cb57096a43ce7969e88d0b9db9c
Rakesh Sarma <rakeshsarma033@gmail.com>
2023-02-08 16:58:53 +0530

    added review section

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Updates from '../Updates/Updates'
+import CustomerReview from '../CustomerReview/CustomerReview'
 import './RightSide.css'
 
 const RightSide = () => {
@@ -8,6 +9,7 @@ const RightSide = () => {
         <div>
             <h3>Updates</h3>
             <Updates/>
+            <CustomerReview/>
         </div>
     </div>
   )
