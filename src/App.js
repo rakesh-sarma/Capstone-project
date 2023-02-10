51d661e5461353388b40d28d9fd152f5ab191e13
Rakesh Sarma <rakeshsarma033@gmail.com>
2023-02-10 11:26:16 +0530

    .

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         <Route path="/" element={<LogIn/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/home" element={<HomePage/>}/>
-        <Route path="/home" element={<HomePage/>}/>
         <Route path="*" element={<div>Error: Not Found the page</div>}/>
       </Routes>
     </div>
